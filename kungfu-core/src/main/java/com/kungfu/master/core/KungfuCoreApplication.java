2a59ef2258738ade7310809f5851f9e5a619650a
mtiaungkyawtin <109195267+mtiaungkyawtin@users.noreply.github.com>
2022-11-24 12:53:44 +0630

    initial commit
    initial commit

---
+++ kungfu-core/src/main/java/com/kungfu/master/core/KungfuCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kungfu.master.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KungfuCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KungfuCoreApplication.class, args);
+	}
+
+}
---
+++ kungfu-core/src/test/java/com/kungfu/master/core/KungfuCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kungfu.master.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KungfuCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
